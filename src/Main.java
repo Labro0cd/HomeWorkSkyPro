98666a8e2c842a882b82d49c752f8bbdf5ea7a23
Kirill <deuse.sec@gmail.com>
2024-09-02 22:26:07 +0300

    HW_String

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-        String firstName = "Ivanov";
-        String middleName = "Ivan";
-        String lastName = "Ivanovich";
-        String fullName = firstName + " " + middleName + " " + lastName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println(fullName);
         // Задача 2
         System.out.println("Данные Ф.И.О сотрудника для заполнения отчета - "+fullName.toUpperCase());
